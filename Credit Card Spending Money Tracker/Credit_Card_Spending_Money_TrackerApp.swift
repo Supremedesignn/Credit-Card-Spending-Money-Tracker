2b13ed9367912f6d5377348a212feb9b7843b928
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-05-28 10:19:33 -0400

    Initial Commit

---
+++ Credit Card Spending Money Tracker/Credit_Card_Spending_Money_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Credit_Card_Spending_Money_TrackerApp.swift
+//  Credit Card Spending Money Tracker
+//
+//  Created by Nkosi Yafeu on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Credit_Card_Spending_Money_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
